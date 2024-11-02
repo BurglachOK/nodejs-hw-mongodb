ea16b12c407fa8bb226f4c1dd772e1eeec5b24ef
Serhii <158744795+BurglachOK@users.noreply.github.com>
2024-11-02 13:48:56 +0100

    hw3_crud

--- src/server.js
+++ src/server.js
@@ -2,14 +2,20 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
-
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
     const app = express();
 
-    app.use(express.json());
+    app.use(
+        express.json({
+            type: ['application/json', 'application/vnd.api+json'],
+            limit: '100kb',
+        }),
+    );
     app.use(cors());
     app.use(
         pino({
@@ -24,39 +30,9 @@ export const setupServer = () => {
             message: 'Hello world!',
         });
     });
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-        res.status(200).json({
-            status: 200,
-            message: 'Successfully found contacts!',
-            data: contacts,
-        });
-    });
-    app.get('/contacts/:contactId', async (req, res, next) => {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
-        if (!contact) {
-            res.status(404).json({ message: 'Contact not found' });
-            return;
-        }
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-    });
-    app.use('*', (req, res, next) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
-
-    app.use((err, req, res, next) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    app.use(contactsRouter);
+    app.use('*', notFoundHandler);
+    app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
