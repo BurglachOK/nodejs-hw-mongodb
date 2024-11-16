9ef5c804b350ec34f54d4ea5eb24c96727ea5edf
Serhii <158744795+BurglachOK@users.noreply.github.com>
2024-11-16 13:47:09 +0100

    .

--- src/services/auth.js
+++ src/services/auth.js
@@ -98,30 +98,30 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
 };
 
 //export const resetPassword = async (payload) => {
-    //let entries;
+//let entries;
 
-    //try {
-      //  entries = jwt.verify(payload.token, env('JWT_SECRET'));
-    //} catch (err) {
-        //if (err instanceof Error) throw createHttpError(401, err.message);
-      //  throw err;
-    //}
+//try {
+//  entries = jwt.verify(payload.token, env('JWT_SECRET'));
+//} catch (err) {
+//if (err instanceof Error) throw createHttpError(401, err.message);
+//  throw err;
+//}
 
-    //const user = await UsersCollection.findOne({
-        //email: entries.email,
-      //  _id: entries.sub,
-    //});
+//const user = await UsersCollection.findOne({
+//email: entries.email,
+//  _id: entries.sub,
+//});
 
-    //if (!user) {
-      //  throw createHttpError(404, 'User not found');
-    //}
+//if (!user) {
+//  throw createHttpError(404, 'User not found');
+//}
 
-    //const encryptedPassword = await bcrypt.hash(payload.password, 10);
+//const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
-    //await UsersCollection.updateOne(
-      //  { _id: user._id },
-    //    { password: encryptedPassword },
-  //  );
+//await UsersCollection.updateOne(
+//  { _id: user._id },
+//    { password: encryptedPassword },
+//  );
 //};
 
 export const resetPassword = async ({ token, password }) => {
