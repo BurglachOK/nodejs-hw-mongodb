38997eca466830a03223cddf99f215d21cf34627
Serhii <158744795+BurglachOK@users.noreply.github.com>
2024-11-17 14:30:20 +0100

    fix

--- ReadME.md
+++ ReadME.md
@@ -1,3 +1,9 @@
 ctrl+C to close localhost
 npm start to start
 npm install t install all dep.
+
+    app.get('/', (req, res) => {
+        res.json({
+            message: 'Welcome to the contacts!',
+        });
+    });
--- src/server.js
+++ src/server.js
@@ -13,12 +13,11 @@ dotenv.config();
 
 const PORT = Number(env('PORT', '3000'));
 
-export function setupServer() {
+export const setupServer = () => {
     const app = express();
-    app.use('/uploads', express.static(UPLOAD_DIR));
+
     app.use(cors());
     app.use(cookieParser());
-    app.use(router);
 
     app.get('/', (req, res) => {
         res.json({
@@ -27,14 +26,16 @@ export function setupServer() {
     });
 
 
-    app.use('*', notFoundHandler);
+    app.use(router);
 
+    app.use(errorHandler);
+    app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
+    app.use('*', notFoundHandler);
     app.use(errorHandler);
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 
-    app.use('/api-docs', swaggerDocs());
-
-}
+};
